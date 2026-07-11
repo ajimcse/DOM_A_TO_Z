80360aa0fa0d7d6387a5d3e615011f28bc83d691
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-11 19:04:34 +0600

    events click

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,15 @@
-// console.log(document.getElementsByTagName('li'))
+
+        const section = document.createElement('section')
+        section.innerHTML = `
+          
+             <h1>My Web Development Skill</h1>
+             <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Tailwind CSS</li>
+                <li>JavaScript</li>
+                <li>React Js</li>
+             </ul>
+        `
+        const skillContainer = document.getElementById('main-container')
+        skillContainer.appendChild(section)
