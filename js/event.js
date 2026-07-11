6e091da7c7ca50fcb06c288f2230357a227a8dfc
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-11 22:42:27 +0600

    event button click a text add kora

--- js/event.js
+++ js/event.js
@@ -1,26 +1,26 @@
-// option 2
-function makeaRed() {
-    document.body.style.backgroundColor = 'red'
-}
-// option 3
-const makeBlueBtn = document.getElementById('make_blue')
-makeBlueBtn.onclick = makeBlue
-function makeBlue() {
-    document.body.style.backgroundColor = 'blue'
-}
-// Option 4
+// // option 2
+// function makeaRed() {
+//     document.body.style.backgroundColor = 'red'
+// }
+// // option 3
+// const makeBlueBtn = document.getElementById('make_blue')
+// makeBlueBtn.onclick = makeBlue
+// function makeBlue() {
+//     document.body.style.backgroundColor = 'blue'
+// }
+// // Option 4
 
-const makePinkButton = document.getElementById('make-pink');
-makePinkButton.addEventListener('click', makePink)
-function makePink() {
-    document.body.style.backgroundColor = 'pink'
-}
-// option 4 sup
-const makeGreenButton = document.getElementById('make-green');
-makeGreenButton.addEventListener('click', function makeGreen() {
-    document.body.style.backgroundColor = 'green'
-})
-// option final 
-document.getElementById('make-goldenrod').addEventListener('click', function () {
-    document.body.style.backgroundColor = 'goldenrod'
-})
+// const makePinkButton = document.getElementById('make-pink');
+// makePinkButton.addEventListener('click', makePink)
+// function makePink() {
+//     document.body.style.backgroundColor = 'pink'
+// }
+// // option 4 sup
+// const makeGreenButton = document.getElementById('make-green');
+// makeGreenButton.addEventListener('click', function makeGreen() {
+//     document.body.style.backgroundColor = 'green'
+// })
+// // option final 
+// document.getElementById('make-goldenrod').addEventListener('click', function () {
+//     document.body.style.backgroundColor = 'goldenrod'
+// })
